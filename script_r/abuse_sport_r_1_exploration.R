1424e57608fd60cb4fcca5ca23803f6b10f781aa
adria4data <141827371+adria4data@users.noreply.github.com>
2023-11-02 13:03:59 -0500

    Describing the objectives

---
+++ script_r/abuse_sport_r_1_exploration.R
@@ -0,0 +1,39 @@
+# Title, Authors & Date ---------------------------------------------------------
+
+
+# Uncovering Shadows: Analyzing Sport Abuse in Romania
+
+# Adrian Rus & Stativa Ecaterina
+
+# November 1, 2023 
+
+
+# Objective ---------------------------------------------------------------------
+
+
+# Objective: Data Preparation and Validation for Initial Phase of Data Analysis
+
+# Description: This objective entails the verification of data integrity, identification and amendment of discrepancies, and the restructuring of data into a format that is conducive for ensuing analytical tasks, ensuring the data is tidy, consistent, and adequately prepared for rigorous analysis.
+
+
+# Packages ---------------------------------------------------------------------
+
+# Here are some widely-used packages from the `tidyverse` ecosystem I will use for 
+
+# Here are some widely-used packages from the `tidyverse` ecosystem I will use for data management and visualization:
+
+# **ggplot2**: For creating visually appealing and informative data visualizations.
+# **dplyr**: For data transformation and manipulation, such as filtering, summarizing, and rearranging data.
+# **tidyr**: For tidying data, making it easier to work with.
+# **readr**: For importing and exporting data.
+# **purrr**: For functional programming and vectorized operations.
+# **stringr**: For string manipulation.
+# **forcats**: For working with categorical data.
+
+
+
+
+
+
+
+
