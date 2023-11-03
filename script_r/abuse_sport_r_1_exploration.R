37ef96b3b0cefd9186191b8ca2fb3b5a28d9a503
adria4data <141827371+adria4data@users.noreply.github.com>
2023-11-03 12:51:46 -0500

    Updated list of packages

--- script_r/abuse_sport_r_1_exploration.R
+++ script_r/abuse_sport_r_1_exploration.R
@@ -8,7 +8,7 @@
 # November 1, 2023 
 
 
-# Objective ---------------------------------------------------------------------
+# 1. Objective ---------------------------------------------------------------------
 
 
 # Objective: Data Preparation and Validation for Initial Phase of Data Analysis
@@ -16,19 +16,24 @@
 # Description: This objective entails the verification of data integrity, identification and amendment of discrepancies, and the restructuring of data into a format that is conducive for ensuing analytical tasks, ensuring the data is tidy, consistent, and adequately prepared for rigorous analysis.
 
 
-# Packages ---------------------------------------------------------------------
-
-# Here are some widely-used packages from the `tidyverse` ecosystem I will use for 
+# 2. Packages ---------------------------------------------------------------------
 
 # Here are some widely-used packages from the `tidyverse` ecosystem I will use for data management and visualization:
 
-# **ggplot2**: For creating visually appealing and informative data visualizations.
-# **dplyr**: For data transformation and manipulation, such as filtering, summarizing, and rearranging data.
-# **tidyr**: For tidying data, making it easier to work with.
-# **readr**: For importing and exporting data.
-# **purrr**: For functional programming and vectorized operations.
-# **stringr**: For string manipulation.
-# **forcats**: For working with categorical data.
+### dplyr (1.1.3): Provides functions for data manipulation and transformation, such as filtering, summarizing, and joining tables.
+### readr (2.1.4): Facilitates importing and exporting data, offering fast and friendly ways to read tabular data into R.
+### forcats (1.0.0): Tools for working with categorical data (factors) in R, including changing factor levels or reordering them.
+### stringr (1.5.0): Simplifies working with strings by providing functions for common string manipulation tasks.
+### ggplot2 (3.4.4): A system for declaratively creating graphics, providing high-level functions for creating visually appealing and informative data visualizations.
+### tibble (3.2.1): Extends R's data frames with better formatting and subsetting functionalities.
+### lubridate (1.9.3): Simplifies working with dates and times by providing functions to parse, manipulate, and do arithmetic with date-times.
+### tidyr (1.3.0): Offers functions to tidy data, reshaping it into a standardized format that's easy to work with.
+### purrr (1.0.2): Provides tools for functional programming and vectorized operations, enabling cleaner and more modular code.
+
+
+### 2.1. Setting Up the Environment: R Packages Installation----------------------
+install.packages("tidyverse")
+library(tidyverse)
 
 
 
