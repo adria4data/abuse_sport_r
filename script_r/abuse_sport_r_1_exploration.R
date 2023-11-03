9918c2daac6d3288adb859f9d92946bff6f53dca
adria4data <141827371+adria4data@users.noreply.github.com>
2023-11-03 13:23:55 -0500

    The steps I follow to prepare my new dataset for analysi.

--- script_r/abuse_sport_r_1_exploration.R
+++ script_r/abuse_sport_r_1_exploration.R
@@ -16,7 +16,8 @@
 # Description: This objective entails the verification of data integrity, identification and amendment of discrepancies, and the restructuring of data into a format that is conducive for ensuing analytical tasks, ensuring the data is tidy, consistent, and adequately prepared for rigorous analysis.
 
 
-# 2. Packages ---------------------------------------------------------------------
+
+# 2. Packages -------------------------------------------------------------------
 
 # Here are some widely-used packages from the `tidyverse` ecosystem I will use for data management and visualization:
 
@@ -31,10 +32,108 @@
 ### purrr (1.0.2): Provides tools for functional programming and vectorized operations, enabling cleaner and more modular code.
 
 
-### 2.1. Setting Up the Environment: R Packages Installation----------------------
+
+### 2.1. Setting Up the Environment: R Packages Installation---------------------
 install.packages("tidyverse")
 library(tidyverse)
 
+
+
+### 2.2. Setting the Working Directory-------------------------------------------
+# Check the working directory
+getwd()
+
+# For setting a new working directory use the setwd()
+# setwd("C:\\Users\\adria\\Documents\\waz_infants")
+
+# List Files in Directory To see a list of all files in the current working directory, you can use
+list.files()
+
+
+
+# 3. Import and check the dataset------------------------------------------------
+
+library(readr)
+abuse_exploration <- read_csv("data/abuse_sports_2_exploration.csv")
+View(abuse_exploration)
+
+
+
+# 4. Exploratory Data Analysis (EDA)---------------------------------------------
+
+# View the first 6 rows
+head(abuse_exploration)
+
+# View the last 6 rows
+tail(abuse_exploration)
+
+# View the structure of dataset
+str(abuse_exploration)
+
+# Summarize the dataset
+summary(abuse_exploration)
+
+
+
+# 5. Data Cleaning---------------------------------------------------------------
+
+## Objectives:
+### Identify and handle missing values using tidyr or dplyr.
+### Convert variable types if necessary using mutate() and functions like as.numeric(), as.factor(), etc.
+### Rename variables for consistency and ease of understanding using rename().
+
+
+
+# 6. Data Transformation---------------------------------------------------------
+  
+## Objectives: 
+### Create new variables or modify existing ones using mutate().
+### Reshape your data between long and wide formats using tidyr.
+### Aggregate or summarize data using summarise() and group_by().
+
+
+
+# 7. Handling Categorical Variables----------------------------------------------
+  
+## Objectives:
+### Use forcats to manage factors, releveling, recoding or collapsing as necessary.
+
+
+
+# 8. Text and Date Manipulation--------------------------------------------------
+  
+# Objectives: If your dataset contains text data, use stringr for string manipulation.
+### For date and time data, use lubridate to simplify working with date-time data.
+
+
+
+# 9. Data Validation-------------------------------------------------------------
+  
+## Objectives:
+### Ensure that your data meets the assumptions and requirements for your planned analyses.
+### Check for outliers, and handle them as appropriate for your analysis goals.
+
+
+
+# 10. Creating Visualizations----------------------------------------------------
+
+## Objectives:
+### Use ggplot2 to create visualizations to further explore and understand your data.
+
+
+
+# 11. Exporting Cleaned Data-----------------------------------------------------
+  
+##  Objectives: 
+### If necessary, export your cleaned and transformed data for further analysis or sharing using readr.
+
+
+# 12. Scripting and Reproducibility----------------------------------------------
+  
+## Objectives:
+### Document your data preparation steps in an R script or R Markdown document to ensure reproducibility.
+### Use comments and create a narrative around your data preparation process to make your work understandable to others, and your future self.
+### By following these steps, and utilizing the tidyverse ecosystem, you can ensure that your data is well-prepared and ready for analysis in a structured and reproducible manner.
 
 
 
